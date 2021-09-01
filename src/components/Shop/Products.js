37acebf48b650b4ee3f4d26ba568c02037aa72ac
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-01 11:20:17 +0300

    applied the template data in components

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,38 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_ITEMS = [
+	{
+		id: 'p1',
+		title: 'My First Book',
+		price: 6,
+		description: 'This Is My First Book Ever Wrote!'
+	},
+	{
+		id: 'p2',
+		title: 'My Second Book',
+		price: 5,
+		description: 'This Is My Second Book Ever Wrote!'
+	}
+];
+
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+	return (
+		<section className={classes.products}>
+			<h2>Buy your favorite products</h2>
+			<ul>
+				{DUMMY_ITEMS.map((item) => (
+					<ProductItem
+						key={item.id}
+						id={item.id}
+						title={item.title}
+						price={item.price}
+						description={item.description}
+					/>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default Products;
