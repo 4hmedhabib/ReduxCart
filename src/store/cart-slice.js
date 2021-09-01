5d2f4c45c32dfe9aac8f86d5c7a15e3008a5b0d0
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-01 10:39:43 +0300

    cart-slice initialized and modified

---
+++ src/store/cart-slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+	name: 'cart',
+	initialState: {
+		items: [],
+		totalQuantity: 0
+	},
+	reducers: {
+		addItemToCart(state, action) {
+			const newItem = action.payload;
+			const existingItem = state.items.find((item) => item.itemId === newItem.id);
+			if (!existingItem) {
+				state.items.push({
+					itemId: newItem.id,
+					name: newItem.title,
+					quantity: newItem.quantity,
+					price: newItem.price,
+					totalPrice: newItem.price
+				});
+			} else {
+				existingItem.quantity++;
+				existingItem.totalPrice = existingItem.price + newItem.price;
+			}
+		}
+	}
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice.reducer;
