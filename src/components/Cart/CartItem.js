e15ba271b026d5561ea073fb8797c6349afe1f7c
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-01 11:48:22 +0300

    added functional and modified functional components

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,28 +1,48 @@
+import { useDispatch } from 'react-redux';
+
+import { cartActions } from '../../store/cart-slice';
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+	const { title, quantity, total, price, id } = props.item;
 
-  return (
-    <li className={classes.item}>
-      <header>
-        <h3>{title}</h3>
-        <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
-        </div>
-      </header>
-      <div className={classes.details}>
-        <div className={classes.quantity}>
-          x <span>{quantity}</span>
-        </div>
-        <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
-        </div>
-      </div>
-    </li>
-  );
+	const dispatch = useDispatch();
+
+	const addToCartHandler = () => {
+		dispatch(
+			cartActions.addItemToCart({
+				id,
+				title,
+				quantity: 1,
+				price
+			})
+		);
+	};
+
+	const removeFromCartHandler = () => {
+		console.log(id);
+		dispatch(cartActions.removeItemFromCart(id));
+	};
+
+	return (
+		<li className={classes.item}>
+			<header>
+				<h3>{title}</h3>
+				<div className={classes.price}>
+					${total.toFixed(2)} <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+				</div>
+			</header>
+			<div className={classes.details}>
+				<div className={classes.quantity}>
+					x <span>{quantity}</span>
+				</div>
+				<div className={classes.actions}>
+					<button onClick={removeFromCartHandler}>-</button>
+					<button onClick={addToCartHandler}>+</button>
+				</div>
+			</div>
+		</li>
+	);
 };
 
 export default CartItem;
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -10,6 +10,7 @@ const cartSlice = createSlice({
 		addItemToCart(state, action) {
 			const newItem = action.payload;
 			const existingItem = state.items.find((item) => item.id === newItem.id);
+			state.totalQuantity++;
 			if (!existingItem) {
 				state.items.push({
 					id: newItem.id,
@@ -20,7 +21,20 @@ const cartSlice = createSlice({
 				});
 			} else {
 				existingItem.quantity++;
-				existingItem.totalPrice = existingItem.price + newItem.price;
+				existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+			}
+		},
+		removeItemFromCart(state, action) {
+			if (state.totalQuantity > 0) {
+				const id = action.payload;
+				const existingItem = state.items.find((item) => item.id === id);
+				state.totalQuantity--;
+				if (existingItem.quantity === 1) {
+					state.items = state.items.filter((item) => item.id !== existingItem.id);
+				} else {
+					existingItem.quantity--;
+					existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+				}
 			}
 		}
 	}
