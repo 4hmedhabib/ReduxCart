b64216faef49f13e06fdd855a57a3f7833cd72c9
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-02 11:28:51 +0300

    added Action Creator Thunk

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 let isInitial = true;
 function App() {
 	const cartShow = useSelector((state) => state.ui.cartIsVisible);
@@ -14,46 +15,12 @@ function App() {
 
 	useEffect(
 		() => {
-			const sendCartData = async () => {
-				dispatch(
-					uiActions.showNotification({
-						status: 'pending',
-						title: 'Pendin...',
-						message: 'Sending Cart Data'
-					})
-				);
-				const response = await fetch('https://react-http-992d0-default-rtdb.firebaseio.com/cart.json', {
-					method: 'PUT',
-					body: JSON.stringify(cart)
-				});
-
-				if (!response.ok) {
-					throw Error('Sending cart data failed!');
-				}
-
-				dispatch(
-					uiActions.showNotification({
-						status: 'success',
-						title: 'Success!',
-						message: 'Sent Cart Data Successfully!'
-					})
-				);
-			};
-
 			if (isInitial) {
 				isInitial = false;
 				return;
 			}
 
-			sendCartData().catch((err) => {
-				dispatch(
-					uiActions.showNotification({
-						status: 'error',
-						title: 'Error!',
-						message: 'Sent Cart Data failed!'
-					})
-				);
-			});
+			dispatch(sendCartData(cart));
 		},
 		[ cart, dispatch ]
 	);
